5ae719dc8fb01572b0fb51c3559862a2ddf529b2
Jurica Kenda <jurica.kenda@gmail.com>
2019-05-17 12:29:44 +0200

    Initializing the project

---
+++ Expense Monitor/src/main/java/com/expenses/Expense/Monitor/ExpenseMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.expenses.Expense.Monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseMonitorApplication.class, args);
+	}
+
+}
