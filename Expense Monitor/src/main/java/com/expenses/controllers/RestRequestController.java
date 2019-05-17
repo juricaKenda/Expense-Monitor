5ee88e4762d1f728d0faf24dd3a77440bad8ffd8
Jurica Kenda <jurica.kenda@gmail.com>
2019-05-17 13:28:25 +0200

    Adding the welcome page mapping and html file

--- Expense Monitor/src/main/java/com/expenses/controllers/RestRequestController.java
+++ Expense Monitor/src/main/java/com/expenses/controllers/RestRequestController.java
@@ -1,13 +1,19 @@
 package com.expenses.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.expenses.mockrepository.MemberRepository;
+
 @RestController
 public class RestRequestController {
+	
+	@Autowired
+	private MemberRepository memberRepo;
 
 	@RequestMapping("/home")
 	public String welcomePage() {
-		return "Welcome";
+		return "welcome-page";
 	}
 }
