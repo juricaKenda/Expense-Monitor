4cc7fea6ee99b559760ae366a36ace28c38af80d
Jurica Kenda <jurica.kenda@gmail.com>
2019-05-17 13:16:43 +0200

    Added the component scan annotation.

--- Expense Monitor/src/main/java/com/expenses/Expense/Monitor/ExpenseMonitorApplication.java
+++ Expense Monitor/src/main/java/com/expenses/Expense/Monitor/ExpenseMonitorApplication.java
@@ -2,8 +2,10 @@ package com.expenses.Expense.Monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.expenses")
 public class ExpenseMonitorApplication {
 
 	public static void main(String[] args) {
