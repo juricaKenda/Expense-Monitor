57a8238150a45ca4b69808eabe3ec27960093631
Jurica Kenda <jurica.kenda@gmail.com>
2019-05-18 12:23:18 +0200

    Adding service tests

---
+++ Expense Monitor/src/test/java/com/expenses/serviceTests/ServiceTestConstants.java
@@ -0,0 +1,14 @@
+package com.expenses.serviceTests;
+
+public class ServiceTestConstants {
+
+	public static final int mockedSenderID = 0;
+	public static final int mockedSenderStartBalance = 10;
+	
+	public static final int mockedReceiverID = 1;
+	public static final int mockedReceiverStartBalance = -10;
+	
+	public static final int mockedTransactionAmount = 5;
+	
+	
+}
---
+++ Expense Monitor/src/test/java/com/expenses/serviceTests/ServiceTests.java
@@ -0,0 +1,71 @@
+package com.expenses.serviceTests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.expenses.mockrepository.MemberRepository;
+import com.expenses.model.GroupMember;
+import com.expenses.model.Transaction;
+import com.expenses.service.Service;
+import com.expenses.service.TransactionLog;
+
+public class ServiceTests {
+
+	private static Service serviceInstance;
+	
+	
+	@BeforeClass
+	public static void setup() {
+		serviceInstance = new Service();
+		serviceInstance.setRepository(new MemberRepository());
+		serviceInstance.setTransactionLog(new TransactionLog());
+	}
+	
+	
+	/**
+	 * This test ensures that senders balance was cutback
+	 * and the receivers balance was incremented by the same amount
+	 */
+	@Test
+	public void performTransactionTest() {
+		
+		//Prepare the components
+		GroupMember sender = new GroupMember();
+		sender.setId(ServiceTestConstants.mockedSenderID);
+		sender.setBudget(ServiceTestConstants.mockedSenderStartBalance);
+		
+		GroupMember receiver = new GroupMember();
+		receiver.setId(ServiceTestConstants.mockedReceiverID);
+		receiver.setBudget(ServiceTestConstants.mockedReceiverStartBalance);
+		
+		MemberRepository mockRepository = serviceInstance.getRepository();
+		mockRepository.addGroupMember(sender);
+		mockRepository.addGroupMember(receiver);
+		
+		Transaction transaction = new Transaction(sender.getId(), receiver.getId(), ServiceTestConstants.mockedTransactionAmount);
+		
+		//Perform the transaction
+		serviceInstance.performTransaction(transaction);
+		
+		//Inspect the result
+		assertEquals(sender.getBudget(),ServiceTestConstants.mockedSenderStartBalance - transaction.getTransactionAmount());
+		assertEquals(receiver.getBudget(),ServiceTestConstants.mockedReceiverStartBalance + transaction.getTransactionAmount());
+		
+	}
+	
+	/**
+	 * Transaction log size should linearly follow
+	 * the amount of transactions performed
+	 */
+	@Test
+	public void getAllTransactionsTest() {
+		performTransactionTest();
+		assertEquals(serviceInstance.getAllTransactions().size(),1);
+		
+		serviceInstance.getAllTransactions().clear();
+		assertEquals(serviceInstance.getAllTransactions().size(),0);
+	}
+	
+}
