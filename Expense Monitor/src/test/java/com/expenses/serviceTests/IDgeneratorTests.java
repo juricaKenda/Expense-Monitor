cca815373d428a4068e65d219b0b97d4565bea03
Jurica Kenda <jurica.kenda@gmail.com>
2019-05-18 12:23:39 +0200

    Adding ID generator tests.

---
+++ Expense Monitor/src/test/java/com/expenses/serviceTests/IDgeneratorTests.java
@@ -0,0 +1,41 @@
+package com.expenses.serviceTests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.expenses.service.IDgenerator;
+
+public class IDgeneratorTests {
+
+	private static IDgenerator generator;
+	private static final int TEST_QUANTITY = 100;
+	
+	@Before
+	public void setup() {
+		generator = new IDgenerator();
+	}
+	
+	
+	/**
+	 * ID generator has to return unique ID 
+	 * each and every time
+	 */
+	@Test
+	public void testAllUnique() {
+		
+		//Prepare
+		ArrayList<Integer> idStorage = new ArrayList<Integer>();
+		
+		//Perform
+		for(int count=0; count< TEST_QUANTITY; count++) {
+			idStorage.add(generator.generateId());
+		}
+		
+		//Assert
+		assertEquals(idStorage.stream().distinct().count(),TEST_QUANTITY);
+	}
+}
