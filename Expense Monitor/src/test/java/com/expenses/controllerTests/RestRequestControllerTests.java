980b5d7e3b0c22bc1ead19482f10ff5dcf339bbc
Jurica Kenda <jurica.kenda@gmail.com>
2019-05-18 17:41:21 +0200

    Adding rest controller test class

---
+++ Expense Monitor/src/test/java/com/expenses/controllerTests/RestRequestControllerTests.java
@@ -0,0 +1,24 @@
+package com.expenses.controllerTests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.expenses.controllers.RestRequestController;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = RestRequestController.class)
+
+public class RestRequestControllerTests {
+	
+	//TODO
+
+}
