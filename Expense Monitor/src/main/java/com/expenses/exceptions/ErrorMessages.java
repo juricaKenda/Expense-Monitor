5d5ea9f2093d86cef28c9155889b5ee1985fbe58
Jurica Kenda <jurica.kenda@gmail.com>
2019-05-19 11:54:59 +0200

    Adding a separate class for error messages

---
+++ Expense Monitor/src/main/java/com/expenses/exceptions/ErrorMessages.java
@@ -0,0 +1,9 @@
+package com.expenses.exceptions;
+
+public class ErrorMessages {
+
+	public static final String INVALID_TRANSACTION_MESSAGE = "Requested transaction is invalid and can not be performed!";
+	public static final String MEMBER_NOT_FOUND_MESSAGE = "Group member with a given ID was not found in the repository!";
+	public static final String REPOSITORY_NOT_INSTANTIATED = "Repository was not yet instantiated!";
+
+}
